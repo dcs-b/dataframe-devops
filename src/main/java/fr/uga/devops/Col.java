accffd00f48ee3af939c63bc1ea8cd354e65ddde
Drayzd <g31plus@gmail.com>
2023-04-26 21:25:01 +0200

    Add skeleton for dataFrame Add full Column creation and operations

---
+++ src/main/java/fr/uga/devops/Col.java
@@ -0,0 +1,141 @@
+package fr.uga.devops;
+
+import java.util.ArrayList;
+
+enum dataType {
+    INT,
+    FLOAT,
+    DOUBLE,
+    BOOL,
+    STRING,
+}
+
+public class Col { //public methods : read_array, get_type
+    private String label;
+    private dataType type;
+    private ArrayList<Object> elems;
+
+
+    public Col(String label, ArrayList<Object> data) {
+        this.label = label;
+        this.elems = new ArrayList<>();
+        int i = 0;
+        if(data.size() == 0){
+            throw new RuntimeException("Empty data column provided");
+        }
+        if(setType(data.get(0))){
+            for(Object o : data){
+                if(checkType(o)){
+                    this.elems.set(i, o);
+                } else {
+                    this.elems.set(i, null); //sets null value if an element is of the wrong type
+                }
+                i = i + 1;
+            }
+        }else {
+            System.out.println("Unrecognized data type - Exiting.");
+        }
+    }
+
+
+    private Boolean setType(Object o){
+
+        if (o instanceof Integer) {
+            this.type = dataType.INT;
+        } else if (o instanceof String) {
+            this.type = dataType.STRING;
+        } else if (o instanceof Double) {
+            this.type = dataType.DOUBLE;
+        }else if (o instanceof Float) {
+            this.type = dataType.FLOAT;
+        }else if (o instanceof Boolean) {
+            this.type = dataType.BOOL;
+        }
+        else {
+            System.out.println("Unrecognized data type - Error.");
+            return false;
+        }
+        return true;
+    }
+
+    private Boolean checkType(Object o){
+        if(this.type == dataType.INT){
+            return o instanceof Integer;
+        } else if(this.type == dataType.STRING){
+            return o instanceof String;
+        } else if(this.type == dataType.DOUBLE){
+            return o instanceof Double;
+        } else if(this.type == dataType.FLOAT){
+            return o instanceof Float;
+        } else if(this.type == dataType.BOOL){
+            return o instanceof Boolean;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public dataType getType(){
+        return this.type;
+    }
+
+    public Object get_elem(int i){
+        return this.elems.get(i);
+    }
+
+
+    // in these methods we cast Objects doubles. this cast is safe because we check the dataType beforehand and
+    // double is stored on 64bits, so no loss of precision takes place.
+
+    public double avg(){
+        double total = 0;
+        if(this.type == dataType.DOUBLE || this.type == dataType.INT || this.type == dataType.FLOAT){
+            for (Object elem : this.elems) {
+                if(elem != null) {
+                    total = total + (double) elem;
+                } //else + 0 i.e. do nothing
+            }
+            return total / elems.size(); // size can't be 0 by design
+        }
+        return 0; // avg for non maths values is 0
+    }
+
+    public double max(){
+        double max = Double.NEGATIVE_INFINITY;
+        if(this.type == dataType.DOUBLE || this.type == dataType.INT || this.type == dataType.FLOAT){
+            for (Object elem : this.elems) {
+                if(elem != null && (double) elem > max) {
+                    max = (double) elem;
+                }
+            }
+            return max;
+        }
+        return 0; // max for non maths values is 0
+    }
+
+    public double min(){
+        double min = Double.POSITIVE_INFINITY;
+        if(this.type == dataType.DOUBLE || this.type == dataType.INT || this.type == dataType.FLOAT){
+            for (Object elem : this.elems) {
+                if(elem != null && (double) elem < min) {
+                    min = (double) elem;
+                }
+            }
+            return min;
+        }
+        return 0; // min for non maths values is 0
+    }
+
+    public double total(){
+        double tot = 0;
+        if(this.type == dataType.DOUBLE || this.type == dataType.INT || this.type == dataType.FLOAT){
+            for (Object elem : this.elems) {
+                if(elem != null ) {
+                    tot = tot + (double) elem;
+                }
+            }
+            return tot;
+        }
+        return 0; // min for non maths values is 0
+    }
+}
--- src/main/java/fr/uga/devops/Dataframe.java
+++ src/main/java/fr/uga/devops/Dataframe.java
@@ -1,8 +1,47 @@
 package fr.uga.devops;
 
-public class Dataframe {
+import java.io.File;
+import java.util.ArrayList;
 
-    Dataframe() {
+public class Dataframe {
+    private ArrayList<Col> cols;
+    private int numCol;
+
+    public Dataframe() {
 
     }
+
+    public void read_csv(File f){
+
+    }
+
+    public void print(){
+
+    }
+
+    public void head(){
+
+    }
+
+    public void tail(){
+
+    }
+
+    public void add_column(){
+
+    }
+
+    public void remove_column(){
+
+    }
+
+    public Dataframe extract_rows(){
+        return this;
+    }
+
+    public Dataframe extract_columns(){
+        return this;
+    }
+
+
 }
