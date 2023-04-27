9207976cf46ffb4ba870d187688dc95b1555257c
dcs-b <81160533+dcs-b@users.noreply.github.com>
2023-04-27 03:06:53 +0200

    Tests

--- src/test/java/fr/uga/devops/DataframeTest.java
+++ src/test/java/fr/uga/devops/DataframeTest.java
@@ -1,11 +1,42 @@
 package fr.uga.devops;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DataframeTest {
+    Dataframe data;
+
+    @Before
+    public void initialize() {
+        data = new Dataframe("example.csv");
+    }
 
     @Test
-    public void test() {
-        new Dataframe();
+    public void testCSVConstructor() {
+        assertNotNull(data);
+    }
+
+    @Test
+    public void testInvalidCSVConstructor() {
+        assertThrows(RuntimeException.class, () -> new Dataframe("example"));
+        assertThrows(RuntimeException.class, () -> new Dataframe("example.pdf"));
+        assertThrows(RuntimeException.class, () -> new Dataframe("example."));
+        assertThrows(RuntimeException.class, () -> new Dataframe(".csv"));
+    }
+
+    @Test
+    public void testEmptyConstructor() {
+        Dataframe data = new Dataframe();
+        assertNotNull(data);
+    }
+
+    @Test
+    public void testRemoveColumns() {
+        for (int i = 1; i <= 15; i++) {
+            data.removeColumn("label" + i);
+        }
+        data.removeColumn("label1");
     }
 }
