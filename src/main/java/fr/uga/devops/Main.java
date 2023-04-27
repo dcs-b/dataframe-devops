3767d31985fa9544a3945c82ccfc8b710182e4d3
dcs-b <81160533+dcs-b@users.noreply.github.com>
2023-04-27 02:28:19 +0200

    Add tests

--- src/main/java/fr/uga/devops/Main.java
+++ src/main/java/fr/uga/devops/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
         System.out.println("Creating dataframe from csv file: ");
-        String csvFile = "resources/example.csv"; // ../../../../../resources
+        String csvFile = "example.csv"; // ../../../../../resources
         Dataframe df = new Dataframe(csvFile);
         df.show();
 
